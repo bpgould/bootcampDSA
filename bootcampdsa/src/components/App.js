e853ba012471da28615e20be48cdaf87b1736ccf
Bennett Gould <bgould@protonmail.com>
2020-11-11 18:07:19 -0500

    router changes

--- bootcampdsa/src/components/App.js
+++ bootcampdsa/src/components/App.js
@@ -1,16 +1,20 @@
-import './App.css';
+import '../App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import TopNav from './TopNav';
 import HomePage from './HomePage';
 import Learn from './Learn';
+import About from './About';
 
 function App() {
   return (
     <Router>
     <div className="App">
       <TopNav />
+      <Switch>
       <Route path='/' component={HomePage} />
+      <Route path='/about' component={About} />
       <Route path='/learn' component={Learn} />
+      </Switch>
     </div>
     </Router>
   );
